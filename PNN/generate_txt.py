713de9ac4843bb9bd1c2e3106e3b091018ea5163
BartWeyder <jackjunior98@gmail.com>
2018-10-15 12:30:01 +0300

    pnn: generated txt for test archives

--- PNN/generate_txt.py
+++ PNN/generate_txt.py
@@ -1,16 +1,18 @@
 from tensorflow.contrib.learn.python.learn.datasets.mnist import extract_images, extract_labels
 import numpy as np
 
-with open('train-images-idx3-ubyte.gz', 'rb') as f:
+#with open('train-images-idx3-ubyte.gz', 'rb') as f:
+with open('t10k-images-idx3-ubyte.gz', 'rb') as f:
 	train_images = extract_images(f)
 
-with open('train-labels-idx1-ubyte.gz', 'rb') as f:
+#with open('train-labels-idx1-ubyte.gz', 'rb') as f:
+with open('t10k-labels-idx1-ubyte.gz', 'rb') as f:
 	train_labels = extract_labels(f)
 
 # norming values
 train_images = train_images.reshape(-1, 28 * 28) / 255.0
 
-with open('train_images.txt', 'w') as f:
+with open('test_images.txt', 'w') as f:
 	i = 0 
 	for ar in train_images:
 		print(i)
@@ -20,4 +22,4 @@ with open('train_images.txt', 'w') as f:
 		np.savetxt(f, ar, '%.2f', delimiter = ';')
 		i += 1
 
-np.savetxt('train_labels.txt', train_labels)
+np.savetxt('test_labels.txt', train_labels)
